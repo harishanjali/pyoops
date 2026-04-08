d4010d5df2a4b40c5e3902427bc57ed9c79ca6f6
harish <harikrishnapuram198@gmail.com>
2026-04-08 12:18:02 +0530

    learned the core behaviour in python

---
+++ core_behaviour_py.py
@@ -0,0 +1,15 @@
+# #isinstnace-whether the object is related to that class or not
+
+# a = 10
+# b = 20
+# c = a+b
+
+# a = 'raju'
+# b = 'krishna'
+# c = a+b
+
+#so here + is behaving different in different situation
+#for integers is adding two numbers
+#for strings its concating
+#how + is doing this
+#whateevr is doing in python, its doing on objects using magic methods
